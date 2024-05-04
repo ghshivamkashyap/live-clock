626aba1d51ea7a627c37afdd93b8a418aea5bca0
SHIVAM KASHYAP <107611606+ghshivamkashyap@users.noreply.github.com>
2024-05-04 16:35:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ function updateClock() {
   let hours = date.getHours().toString().padStart(2, 0);
   let minutes = date.getMinutes().toString().padStart(2, 0);
   let seconds = date.getSeconds().toString().padStart(2, 0);
-  id.textContent = `${hours}:${minutes}:${seconds}`;
+  let amPm;
+  if (hours > 12) {
+    amPm = "PM";
+  } else {
+    amPm = "AM";
+  }
+
+  id.textContent = hours < 12 ? `${hours}:${minutes}:${seconds} ${amPm}` :  `${hours-12}:${minutes}:${seconds} ${amPm}`;
+    //  id.textContent = hours < 12 ? `${hours}:${minutes}:${seconds} ${amPm}` :  `${hours}:${minutes}:${seconds} ${amPm}`;
+
 }
 
-// setTimeout(() => {
-//   updateClock();
-// }, 1000);
+setTimeout(() => {
+  updateClock();
+}, 1000);
 setInterval(() => {
   updateClock();
 }, 1000);
